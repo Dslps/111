a1538af1f3ceba3b30d472167b58b72bf5fba30a
Danis Petrus <d.petrus@lotta.local>
2024-11-21 17:55:24 +0500

    danis

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
                     <i class="fa-solid fa-lock top-contact-icon"></i>
                     <div class="lg:my-0">Licensed, Bonded and Insured</div>
                 </div>
-        
+
                 <!-- Правая часть -->
                 <div class="top-contact-right">
                     <div class="top-contact-item mr-0 lg:mr-5">
@@ -35,17 +35,17 @@
                 </div>
             </div>
         </div>
-        
+
         {{-- навигационная панель --}}
         <div class="navigation-box">
             <div class="navigation-container">
                 <!-- Логотип -->
                 <div class="logo-container">
                     <a href="">
-                        <img class="logo-image" src="{{asset('img/Logo.png')}}" alt="Logo">
+                        <img class="logo-image" src="{{ asset('img/Logo.png') }}" alt="Logo">
                     </a>
                 </div>
-        
+
                 <!-- Навигация -->
                 <div class="nav-menu">
                     <button class="burger-menu" aria-label="Toggle navigation">
@@ -57,7 +57,8 @@
                             <a href="#" class="nav-link flex justify-between lg:pt-0 pt-5">
                                 HANDYMAN
                                 <div>
-                                    <i class="fa-solid fa-angle-down toggle-dropdown lg:ml-[5px] transition-transform duration-300"></i>
+                                    <i
+                                        class="fa-solid fa-angle-down toggle-dropdown lg:ml-[5px] transition-transform duration-300"></i>
                                 </div>
                             </a>
                             <ul class="dropdown-menu">
@@ -94,16 +95,17 @@
                                 </li>
                             </ul>
                         </li>
-                        
-            
+
+
                         <!-- REMODELING -->
                         <li class="nav-item group">
                             <a href="#" class="nav-link flex justify-between">
                                 REMODELING
                                 <div>
-                                    <i class="fa-solid fa-angle-down toggle-dropdown lg:ml-[5px] transition-transform duration-300"></i>
+                                    <i
+                                        class="fa-solid fa-angle-down toggle-dropdown lg:ml-[5px] transition-transform duration-300"></i>
                                 </div>
-                                
+
                             </a>
                             <ul class="dropdown-menu">
                                 <li class="dropdown-item">
@@ -127,15 +129,16 @@
                                 </li>
                             </ul>
                         </li>
-            
+
                         <!-- EXTERIOR -->
                         <li class="nav-item group">
                             <a href="#" class="nav-link flex justify-between">
                                 EXTERIOR
                                 <div>
-                                    <i class="fa-solid fa-angle-down toggle-dropdown lg:ml-[5px] transition-transform duration-300"></i>
+                                    <i
+                                        class="fa-solid fa-angle-down toggle-dropdown lg:ml-[5px] transition-transform duration-300"></i>
                                 </div>
-                                
+
                             </a>
                             <ul class="dropdown-menu">
                                 <li class="dropdown-item">
@@ -159,15 +162,16 @@
                                 </li>
                             </ul>
                         </li>
-            
+
                         <!-- ABOUT US -->
                         <li class="nav-item group">
                             <a href="#" class="nav-link flex justify-between">
                                 ABOUT US
                                 <div>
-                                    <i class="fa-solid fa-angle-down toggle-dropdown lg:ml-[5px] transition-transform duration-300"></i>
+                                    <i
+                                        class="fa-solid fa-angle-down toggle-dropdown lg:ml-[5px] transition-transform duration-300"></i>
                                 </div>
-                                
+
                             </a>
                             <ul class="dropdown-menu">
                                 <li class="dropdown-item">
@@ -195,19 +199,109 @@
                                 </li>
                             </ul>
                         </li>
-            
+
                         <!-- OTHER ITEMS -->
-                        <li class="nav-item group pb-[10px] lg:pb-0"><a href="#" class="nav-link">FEATURED PROJECT</a></li>
-                        <li class="nav-item group pb-[10px] lg:pb-0"><a href="#" class="nav-link">OUR SYSTEM COMPANY</a></li>
-                        <li class="nav-item group pb-[10px] lg:pb-0"><a href="#" class="nav-link">CONTACT US</a></li>
+                        <li class="nav-item group pb-[10px] lg:pb-0"><a href="#" class="nav-link">FEATURED
+                                PROJECT</a></li>
+                        <li class="nav-item group pb-[10px] lg:pb-0"><a href="#" class="nav-link">OUR SYSTEM
+                                COMPANY</a></li>
+                        <li class="nav-item group pb-[10px] lg:pb-0"><a href="#" class="nav-link">CONTACT
+                                US</a></li>
                     </ul>
                 </div>
             </div>
         </div>
-        
+
     </header>
 
     @yield('content')
+
+    <footer class="footer bg-[var(--support-color)] w-full">
+        <div class="footer-container max-w-[1600px] text-[var(--color)] mx-auto flex lg:flex-nowrap flex-wrap  py-[50px]">
+            {{---------------------------- 1 -------------------------}}
+            <div class="footer-column">
+                <p class="footer-title">
+                    CONTACT
+                </p>
+                <p class="footer-text py-2">
+                    <a href="#">(865) 966-7648</a>
+                </p>
+                <p class="footer-text py-2">sales@smithhandymanservice.com</p>
+                <p class="footer-text py-2">1544 Schaeffer Rd. Suite A, Knoxville, TN 37932</p>
+                <p class="footer-title">
+                    CARRERS
+                </p>
+                <p class="footer-text py-2">We're always looking to grow our team.</p>
+                <button class="footer-button">
+                    View jops
+                </button>
+            </div>
+            {{--------------------- 2 -----------------------------------}}
+            <div class="footer-column">
+                <p class="footer-title">
+                    Our services
+                </p>
+                <ul class="footer-list list-disc pl-5">
+                    <li><a href="#">Carpentry</a></li>
+                    <li><a href="#">Gutter installation & repair</a></li>
+                    <li><a href="#">Drywall repair</a></li>
+                    <li><a href="#">Commercial maintenance</a></li>
+                    <li><a href="#">Remodeling</a></li>
+                    <li><a href="#">Home repairs</a></li>
+                    <li><a href="#">Flooring</a></li>
+                    <li><a href="#">Gutter cleaning</a></li>
+                    <li><a href="#">Bathroom remodeling</a></li>
+                    <li><a href="#">Furniture assembly</a></li>
+                    <li><a href="#">Roofing</a></li>
+                    <li><a href="#">Painting</a></li>
+                    <li><a href="#">Plumbing</a></li>
+                </ul>
+            </div>
+            {{------------------------------------ 3 ------------------------------------}}
+            <div class="footer-column">
+                <p class="footer-title">
+                    Where we serve
+                </p>
+                <ul class="footer-list">
+                    <li><i class="fa-solid fa-location-dot location-icon"></i>Knoxville</li>
+                    <li><i class="fa-solid fa-location-dot location-icon"></i>Lenoir City</li>
+                    <li><i class="fa-solid fa-location-dot location-icon"></i>Oak Ridge</li>
+                    <li><i class="fa-solid fa-location-dot location-icon"></i>Maryville</li>
+                    <li><i class="fa-solid fa-location-dot location-icon"></i>Kingston</li>
+                    <li><i class="fa-solid fa-location-dot location-icon"></i>Powell</li>
+                    <li><i class="fa-solid fa-location-dot location-icon"></i>Crossville</li>
+                    <li><i class="fa-solid fa-location-dot location-icon"></i>Sevierville</li>
+                    <li><i class="fa-solid fa-location-dot location-icon"></i>and surrounding areas</li>
+                </ul>
+                <p class="footer-title">
+                    Licensed & insured
+                </p>
+                <p class="footer-text py-2">General Contractors License #76231</p>
+            </div>
+            {{---------------------------------------- 4 ----------------------------------------}}
+            <div class="footer-column"></div>
+        </div>
+        {{----------------------- Footer Bottom -------------------------}}
+        <div class="footer-bottom max-w-[1600px] m-auto text-[var(--color)] pb-10 block">
+            <div class="footer-social flex items-center pb-3 border-b-2 border-[var(--accent-color)]">
+                <div class="footer-social-label mr-5">
+                    Connect with us:
+                </div>
+                <div class="footer-social-icons flex gap-5">
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-google"></i>
+                    <i class="fa-brands fa-tiktok"></i>
+                </div>
+            </div>
+            <div class="footer-links mt-3 space-y-3 block">
+                <p>Copyright © 2024 <a href="">Smith Handyman Service</a> | 1544 Schaeffer Rd. Suite A, Knoxville, TN 37932 | <a href="">(865) 966-7648</a></p>
+                <p><a href="">Powered by Alchemy Marketing</a> | <a href="">Privacy Policy</a> | <a href="">Sitemap</a> | <a href="">Accessibility</a></p>
+            </div>
+        </div>
+    </footer>
+    
+
 
 
 </body>
